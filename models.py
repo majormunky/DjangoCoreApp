a30dee2b42c0ae5f6874270ab22364be3c3861a2
Josh Bright <josh.bright@gmail.com>
2020-02-17 17:39:59 -0800

    Fixed a big issue with how permissions work with a custom user model

--- models.py
+++ models.py
@@ -1,66 +1,59 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class UserPreferences(models.Model):
+    location = models.ForeignKey(
+        Location, blank=True, null=True, on_delete=models.SET_NULL
+    )
+    timezone = models.CharField(max_length=64, blank=True, null=True)
 
 
 class CoreUserManager(BaseUserManager):
-    def create_user(self, email, first_name, last_name, password=None):
-        """
-        Creates a basic user that just needs the email
-        """
+    def _create_user(self, email, password, **extra_fields):
         if not email:
-            raise ValueError("User must have an email address")
+            raise ValueError("Email is required")
 
-        user = self.model(
-            email=self.normalize_email(email),
-            first_name=first_name,
-            last_name=last_name,
-        )
+        user_prefs = UserPreferences()
+        user_prefs.save()
 
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.user_prefs = user_prefs
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, first_name, last_name, password):
-        """
-        Creates a super user that just needs email
-        """
-        user = self.create_user(email, first_name, last_name, password)
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+
+        return self._create_user(email, password, **extra_fields)
 
 
-class CoreUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(verbose_name="email address", max_length=255, unique=True)
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
-    first_name = models.CharField(max_length=64)
-    last_name = models.CharField(max_length=64)
+class CoreUser(AbstractUser):
+    username = None
+    email = models.EmailField("email address", unique=True)
+    preferences = models.OneToOneField(
+        UserPreferences, on_delete=models.CASCADE, blank=True, null=True
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
     objects = CoreUserManager()
 
-    USERNAME_FIELD = "email"
-    EMAIL_FIELD = "email"
-    REQUIRED_FIELDS = ["first_name", "last_name"]
-
-
     def __str__(self):
         return self.email
-
-    def has_perm(self, perm, obj=None):
-        # "Does the user have a specific permission?"
-        # Simplest possible answer: Yes, always
-        return True
-
-    def has_module_perms(self, app_label):
-        # "Does the user have permissions to view the app `app_label`?"
-        # Simplest possible answer: Yes, always
-        return True
-
-    def get_full_name(self):
-        return f"{self.first_name} {self.last_name}"
-
-    @property
-    def is_staff(self):
-        # Simplest possible answer: All admins are staff
-        return self.is_admin
