8c644fdde3c36a17e4c9b50e69c60fcafe16f4e2
Josh Bright <josh.bright@gmail.com>
2023-04-17 22:20:37 -0700

    Fixed test

--- tests/test_models.py
+++ tests/test_models.py
@@ -19,7 +19,7 @@ class ModelTests(TestCase):
         sample_emails = [
             ["test1@EXAMPLE.com", "test1@example.com"],
             ["Test2@Example.com", "Test2@example.com"],
-            ["TEST3@EXAMPLE.COM", "test3@example.com"],
+            ["TEST3@EXAMPLE.COM", "TEST3@example.com"],
         ]
 
         for email, expected in sample_emails:
